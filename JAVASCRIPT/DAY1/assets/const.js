2ab974b485968d5292b304f407414f1d59b27779
manoj <m64938096@gmail.com>
2026-06-08 01:47:10 +0530

    update

--- JAVASCRIPT/DAY1/assets/const.js
+++ JAVASCRIPT/DAY1/assets/const.js
@@ -10,6 +10,89 @@ console.log(paperlaptop);
 const villanfire;
 console.log(villanfire);
 
+const screenchair;
+console.log(screenchair);
+
+const kitchenslipper;
+console.log(kitchenslipper);
+
+const foodplate;
+console.log(foodplate);
+
+const bottlepaper;
+console.log(bottlepaper);
+
+const newspen;
+console.log(newspen);
+
+const netwater;
+console.log(netwater);
+
+const mirrorleaf;
+console.log(mirrorleaf);
+
+const casecat;
+console.log(casecat);
+
+const pepperland;
+console.log(pepperland);
+  
+const dustrice;
+console.log(dustrice);
+
+const tigermango;
+console.log(tigermango);
+
+const ringbanner;
+console.log(ringbanner);
+
+const flowerclock;
+console.log(flowerclock);
+
+const singdass;
+console.log(singdass);
+
+const windowcome;
+console.log(windowcome);
+
+const softclear;
+console.log(softclear);
+
+const filelight;
+console.log(filelight);
+
+const textcan;
+console.log(textcan);
+
+const projectboot;
+console.log(projectboot);
+
+const frontedit;
+console.log(frontedit);
+
+const internalwind;
+console.log(internalwind);
+
+const highscript;
+console.log(highscript);
+
+const manyout;
+console.log(manyout);
+
+const topsand;
+console.log(topsand);
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
