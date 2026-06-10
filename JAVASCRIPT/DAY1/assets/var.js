e7f7d5bf6c5461b4a95ab1c2bf558e87c3e15392
manoj <m64938096@gmail.com>
2026-06-11 00:51:12 +0530

    update all

---
+++ JAVASCRIPT/DAY1/assets/var.js
@@ -0,0 +1,109 @@
+var tablephone;
+console.log(tablephone);
+
+var cargate;
+console.log(cargate);
+
+var manbag;
+console.log(manbag);
+
+var numbervan;
+console.log(numbervan);
+
+var madhanmask;
+console.log(madhanmask);
+
+var riverdoor;
+console.log(riverdoor);
+
+var cabletiles;
+console.log(cabletiles);
+
+var airshirt;
+console.log(airshirt);
+
+var remoteroom;
+console.log(remoteroom);
+
+var bedwatch;
+console.log(bedwatch);
+
+var ninjafan;
+console.log(ninjafan);
+
+var creamhanger;
+console.log(creamhanger);
+
+var benchtree;
+console.log(benchtree);
+
+var comeshot;
+console.log(comeshot);
+
+var screenchair;
+console.log(screenchair);
+
+var kitchenslipper;
+console.log(kitchenslipper);
+
+var foodplate;
+console.log(foodplate);
+
+var bottlepaper;
+console.log(bottlepaper);
+
+var newspen;
+console.log(newspen);
+
+var netwater;
+console.log(netwater);
+
+var mirrorleaf;
+console.log(mirrorleaf);
+
+var casecat;
+console.log(casecat);
+
+var pepperland;
+console.log(pepperland);
+
+var dustrice;
+console.log(dustrice);
+
+var tigermango;
+console.log(tigermango);
+
+var ringbanner;
+console.log(ringbanner);
+
+var flowerclock;
+console.log(flowerclock);
+
+var 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
